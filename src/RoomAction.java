6e4e6ddd43746dab41be04b5d346c438c2e1e09e
zzy <zzy_xpy@163.com>
2017-01-01 15:45:53 +0800

    增加struts_example.xml, 里面有struts配置json插架的示例

--- src/RoomAction.java
+++ src/RoomAction.java
@@ -4,9 +4,27 @@ import com.opensymphony.xwork2.ActionSupport;
  * Created by zhao on 2017/1/1.
  */
 public class RoomAction extends ActionSupport{
+    private DataProcess dp = new DataProcess();
+    private String json = "";
+
+    public DataProcess getDp() {
+        return dp;
+    }
+
+    public void setDp(DataProcess dp) {
+        this.dp = dp;
+    }
+
+    public String getJson() {
+        return json;
+    }
+
+    public void setJson(String json) {
+        this.json = json;
+    }
 
     public String getRoomDetail() {
-
+        json = dp.getData("");
         return SUCCESS;
     }
 }
