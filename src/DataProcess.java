cfbb7e9d64d7985eeb0d991008402d8a8ae12516
zzy <zzy_xpy@163.com>
2017-01-01 15:21:46 +0800

    简单的后台框架，准备开始协作

---
+++ src/DataProcess.java
@@ -0,0 +1,10 @@
+/**
+ * Created by zhao on 2017/1/1.
+ */
+public class DataProcess {
+
+    // 从文件读数据，返回可编码为json的串
+    public String getData(String filename) {
+        return "";
+    }
+}
---
+++ src/RoomAction.java
@@ -0,0 +1,12 @@
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * Created by zhao on 2017/1/1.
+ */
+public class RoomAction extends ActionSupport{
+
+    public String getRoomDetail() {
+        
+        return SUCCESS;
+    }
+}
